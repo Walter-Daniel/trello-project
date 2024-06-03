5dafa5f524b94790374f5b0607aa0e1d2c2c5343
Walter <walterdcarrizo.19@gmail.com>
2024-06-03 13:37:17 -0300

    add getlist action

--- src/app/(plataform)/(dashboard)/organization/[organizationId]/page.tsx
+++ src/app/(plataform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,8 +1,18 @@
+import { getList } from "@/actions/get-list";
 
+interface WorkSpace {
+  params : {
+    organizationId: string;
+  }
+}
 
-export default async function OrganizationIdPage() {
+export default async function OrganizationIdPage({params}:WorkSpace) {
 
-  
+  console.log(params.organizationId)
+
+  const list = await getList(params.organizationId)
+
+  console.log(list)
 
 
   return (
